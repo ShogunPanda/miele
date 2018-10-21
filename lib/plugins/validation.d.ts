3a6cf2aebcf782121b3e01c4a977cf8600385e99
Shogun <shogun@cowtech.it>
2018-10-21 09:27:23 +0200

    feat: Added custom validation format support.

--- lib/plugins/validation.d.ts
+++ lib/plugins/validation.d.ts
@@ -1,7 +1,16 @@
-/// <reference types="fastify" />
 import * as Ajv from 'ajv';
 import Boom from 'boom';
+import * as fastify from 'fastify';
 import { Schema } from '../spec';
+export declare type ValidationPlugin = fastify.Plugin<{}, {}, {}, {}> & {
+    addFormats?: (formatters: CustomValidationFormatters, messages?: CustomValidationMessages) => void;
+};
+export interface CustomValidationFormatters {
+    [key: string]: (raw: string) => boolean;
+}
+export declare type CustomValidationMessages = {
+    [key: string]: string;
+};
 export declare type validationFormatter = (...args: Array<any>) => string;
 export declare const validationMessagesFormatter: {
     [key: string]: validationFormatter;
@@ -10,4 +19,4 @@ export declare const validationMessages: {
     [key: string]: string;
 };
 export declare function convertValidationErrors(data: Schema, validationErrors: Array<Ajv.ErrorObject>, prefix: string, stripPrefix?: RegExp): Boom;
-export declare const customValidationPlugin: import("fastify").Plugin<{}, {}, {}, {}>;
+export declare const customValidationPlugin: ValidationPlugin;
--- src/plugins/validation.ts
+++ src/plugins/validation.ts
@@ -1,5 +1,6 @@
 import * as Ajv from 'ajv'
 import Boom, { badData, internal } from 'boom'
+import * as fastify from 'fastify'
 import { ServerResponse } from 'http'
 import { INTERNAL_SERVER_ERROR } from 'http-status-codes'
 import { get, omit } from 'lodash'
@@ -7,6 +8,16 @@ import { DateTime } from 'luxon'
 import { DecoratedFastify, DecoratedReply, DecoratedRequest, niceJoin } from '../environment'
 import { Route, Schema } from '../spec'
 import { createPlugin } from './utils'
+
+export type ValidationPlugin = fastify.Plugin<{}, {}, {}, {}> & {
+  addFormats?: (formatters: CustomValidationFormatters, messages?: CustomValidationMessages) => void
+}
+
+export interface CustomValidationFormatters {
+  [key: string]: (raw: string) => boolean
+}
+
+export type CustomValidationMessages = { [key: string]: string }
 
 export type validationFormatter = (...args: Array<any>) => string
 
@@ -130,85 +141,100 @@ export function convertValidationErrors(
   return badData('Bad input data.', { errors: prefix ? { [prefix]: errors } : errors })
 }
 
-export const customValidationPlugin = createPlugin(async function(instance: DecoratedFastify): Promise<void> {
-  const ajv = new Ajv({
-    // the fastify defaults
-    removeAdditional: false,
-    useDefaults: true,
-    coerceTypes: true,
-    allErrors: true,
-    unknownFormats: true,
-    // Add custom validation
-    formats: {
-      'date-time'(raw: string): boolean {
-        const parsed = DateTime.fromISO(raw, { zone: 'utc' })
+export const customValidationPlugin = (function(): ValidationPlugin {
+  const customFormats: { [key: string]: (raw: string) => boolean } = {}
 
-        return parsed.isValid
-      }
-    }
-  })
+  const plugin: ValidationPlugin = createPlugin(async function(instance: DecoratedFastify): Promise<void> {
+    const ajv = new Ajv({
+      // the fastify defaults
+      removeAdditional: false,
+      useDefaults: true,
+      coerceTypes: true,
+      allErrors: true,
+      unknownFormats: true,
+      // Add custom validation
+      formats: {
+        'date-time'(raw: string): boolean {
+          const parsed = DateTime.fromISO(raw, { zone: 'utc' })
 
-  // Assign to Fastify
-  instance.setSchemaCompiler(function(schema) {
-    return ajv.compile(schema)
-  })
-
-  // Validate routes responses, only on in development
-  if (instance.environment === 'development') {
-    instance.addHook('onRoute', (routeOptions: Route) => {
-      const responses: Schema | null = get(routeOptions, 'schema.response', null)
-
-      if (responses) {
-        routeOptions.config = routeOptions.config || {}
-        routeOptions.config.responsesValidator = Object.entries(responses).reduce(
-          (accu: Schema, [code, schema]: [string, any]) => {
-            if (schema.raw || schema.empty) {
-              accu[code.toString()] = () => true
-              accu[code.toString()].raw = true
-            } else {
-              const components = get(schema, 'components')
-              const body = omit(schema, 'components')
-
-              accu[code.toString()] = ajv.compile({
-                type: 'object',
-                properties: { body },
-                components
-              })
-            }
-
-            return accu
-          },
-          {}
-        )
+          return parsed.isValid
+        },
+        ...customFormats
       }
     })
-  }
 
-  instance.addHook('onSend', async (_req: DecoratedRequest, reply: DecoratedReply<ServerResponse>, payload: string) => {
-    // Do not re-validate the 500
-    if (reply.res.statusCode === INTERNAL_SERVER_ERROR) return payload
+    // Assign to Fastify
+    instance.setSchemaCompiler(function(schema) {
+      return ajv.compile(schema)
+    })
 
-    const responsesValidator: { [key: string]: Ajv.ValidateFunction & { raw?: boolean } } | null =
-      reply.context.config.responsesValidator
+    // Validate routes responses, only on in development
+    if (instance.environment === 'development') {
+      instance.addHook('onRoute', (routeOptions: Route) => {
+        const responses: Schema | null = get(routeOptions, 'schema.response', null)
 
-    if (responsesValidator) {
-      const code = reply.res.statusCode
-      const validator = responsesValidator[code.toString()]
+        if (responses) {
+          routeOptions.config = routeOptions.config || {}
+          routeOptions.config.responsesValidator = Object.entries(responses).reduce(
+            (accu: Schema, [code, schema]: [string, any]) => {
+              if (schema.raw || schema.empty) {
+                accu[code.toString()] = () => true
+                accu[code.toString()].raw = true
+              } else {
+                const components = get(schema, 'components')
+                const body = omit(schema, 'components')
 
-      // No validator found, it means the status code is invalid
-      if (!validator) throw internal('', { message: validationMessagesFormatter.invalidResponseCode(code) })
+                accu[code.toString()] = ajv.compile({
+                  type: 'object',
+                  properties: { body },
+                  components
+                })
+              }
 
-      const data = { body: validator.raw ? payload : JSON.parse(payload) }
-      const valid = validator(data)
-
-      if (!valid) {
-        throw internal('', {
-          message: validationMessagesFormatter.invalidResponse(code),
-          errors: convertValidationErrors(data, validator.errors!, 'response', /^body\./).data.errors
-        })
-      }
+              return accu
+            },
+            {}
+          )
+        }
+      })
     }
 
-    return payload
+    instance.addHook(
+      'onSend',
+      async (_req: DecoratedRequest, reply: DecoratedReply<ServerResponse>, payload: string) => {
+        // Do not re-validate the 500
+        if (reply.res.statusCode === INTERNAL_SERVER_ERROR) return payload
+
+        const responsesValidator: { [key: string]: Ajv.ValidateFunction & { raw?: boolean } } | null =
+          reply.context.config.responsesValidator
+
+        if (responsesValidator) {
+          const code = reply.res.statusCode
+          const validator = responsesValidator[code.toString()]
+
+          // No validator found, it means the status code is invalid
+          if (!validator) throw internal('', { message: validationMessagesFormatter.invalidResponseCode(code) })
+
+          const data = { body: validator.raw ? payload : JSON.parse(payload) }
+          const valid = validator(data)
+
+          if (!valid) {
+            throw internal('', {
+              message: validationMessagesFormatter.invalidResponse(code),
+              errors: convertValidationErrors(data, validator.errors!, 'response', /^body\./).data.errors
+            })
+          }
+        }
+
+        return payload
+      }
+    )
   })
-})
+
+  plugin.addFormats = function(validators: CustomValidationFormatters, messages?: CustomValidationMessages): void {
+    Object.assign(customFormats, validators)
+    Object.assign(validationMessages, messages || {})
+  }
+
+  return plugin
+})()
