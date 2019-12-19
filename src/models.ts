308570665bcf137d749a357313a7cf031e190e24
Shogun <shogun@cowtech.it>
2019-12-19 11:07:03 +0100

    chore: Rebuild.

--- src/models.ts
+++ src/models.ts
@@ -3,7 +3,3 @@ import { IncomingMessage, ServerResponse } from 'http'
 
 export type Request = FastifyRequest<IncomingMessage>
 export type Reply<T = ServerResponse> = FastifyReply<T>
-export interface DecoratedReply<T = ServerResponse> extends Reply<T> {
-  requestId: number
-  startTime: [number, number]
-}
--- types/models.d.ts
+++ types/models.d.ts
@@ -3,7 +3,3 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { IncomingMessage, ServerResponse } from 'http';
 export declare type Request = FastifyRequest<IncomingMessage>;
 export declare type Reply<T = ServerResponse> = FastifyReply<T>;
-export interface DecoratedReply<T = ServerResponse> extends Reply<T> {
-    requestId: number;
-    startTime: [number, number];
-}
